ea4eb55cf7e6ce81f321bcb20d9d241c1f963b4c
Thomas Pappas <getekid@gmail.com>
2018-06-16 20:41:17 +0300

    SquareGen: Initated AntiMagicSquareGenerator class

---
+++ AntiMagicSquareGenerator.py
@@ -0,0 +1,45 @@
+from sympy.combinatorics import Permutation
+from SymmetricGroupGeneratorsConstructor import SymmetricGroupGeneratorsConstructor
+from math import sqrt, ceil
+
+
+class AntiMagicSquareGenerator(object):
+
+    # As we will need to go through all possible permutations of a square with numbers,
+    # we will initiate the class by identifying a Symmetric Group of degree equal to
+    # the multitude of the possible numbers in the square.
+    #
+    # See the SymmetricGroupGeneratorsConstructor class and its references for more
+    # information on why the generators can be a 2 and 3 order elements.
+    def __init__(self, degree_or_generators):
+        error_class_start = 'Error at initiating the class: '
+        # The parameter can either be the degree (integer) or the generators (tuple of Permutations)
+        if isinstance(degree_or_generators, int):
+            degree = degree_or_generators
+            # Validate the degree
+            if degree < 1:
+                raise RuntimeError(error_class_start + 'Degree needs to be greater than 0.')
+            # Construct the generators
+            constructor = SymmetricGroupGeneratorsConstructor(degree)
+            generators = constructor.construct()
+        elif isinstance(degree_or_generators, tuple):
+            generators = degree_or_generators
+            # Validate the generators
+            if len(generators) != 2 or not isinstance(generators[0], Permutation) or not isinstance(generators[1], Permutation):
+                raise RuntimeError(error_class_start + 'Generators need to be a tuple of two Permutations.')
+            if generators[0].size != generators[1].size:
+                raise RuntimeError(error_class_start + 'Generators need to have the same degree.')
+            # Get the degree from a generator
+            degree = generators[0].size
+        else:
+            raise RuntimeError(
+                error_class_start + 'Provide either a positive integer or a tuple of the two Permutation generators')
+
+        # The Symmetric Group defined needs to represent a Square
+        square_size = sqrt(degree)
+        if ceil(square_size) != square_size:
+            raise RuntimeError(error_class_start + 'Degree needs to be a square number.')
+
+        # Set the Symmetric Group's degree and generators that identify it
+        self.degree = degree
+        self.generators = generators
